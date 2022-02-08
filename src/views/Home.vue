47cecf1e7e16fded6a554221fa4f5be9bf4aecc4
二齐 <40307204@qq.com>
2022-02-08 17:54:20 +0800

    尝试修复自动跳转回首页的问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -961,16 +961,16 @@ export default class Home extends Vue {
     }
 
     async toHomePage() {
-        if (!this.isOwner && this.isOwnerValidRSS3) {
-            const ownerRNS = await RNSUtils.addr2Name(this.ownerETHAddress);
-            if (ownerRNS) {
-                window.location.href = '//' + ownerRNS + '.' + legacyConfig.subDomain.rootDomain;
-            } else {
-                await this.$router.push(`/${this.ownerETHAddress}`);
-            }
+        // if (!this.isOwner && this.isOwnerValidRSS3) {
+        const ownerRNS = await RNSUtils.addr2Name(this.ownerETHAddress);
+        if (ownerRNS) {
+            window.location.href = '//' + ownerRNS + '.' + legacyConfig.subDomain.rootDomain;
         } else {
-            window.location.href = '//' + legacyConfig.subDomain.rootDomain;
+            await this.$router.push(`/${this.ownerETHAddress}`);
         }
+        // } else {
+        //     window.location.href = '//' + legacyConfig.subDomain.rootDomain;
+        // }
     }
 
     toContentLink(content: any) {
